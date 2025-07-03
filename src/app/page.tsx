93c0b90d767320473fc845f826bc6d4d97d05710
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-03 19:53:24 -0300

    feat(nextjs): criação do projeto Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="flex flex-col gap-7 items-center justify-center py-7">
+      <h1 className="text-3xl font-bold">Conversor de Moedas</h1>
+    </main>
+  )
+}
