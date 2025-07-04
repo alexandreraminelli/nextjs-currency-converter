2f77f42e320fa3012f067a8e1696cac9637b136e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-04 14:30:24 -0300

    feat(heroui): adição de provedor

---
+++ src/app/providers.tsx
@@ -0,0 +1,10 @@
+import { HeroUIProvider } from "@heroui/react"
+import { ThemeProvider } from "next-themes"
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <HeroUIProvider>
+      <ThemeProvider attribute="class">{children}</ThemeProvider>
+    </HeroUIProvider>
+  )
+}
