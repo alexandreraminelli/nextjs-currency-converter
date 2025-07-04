9ad24667ead3b06bcf68801f7324825e48032fa5
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-04 19:22:40 -0300

    config(heroui/toast): adição do Toast e definição da posição

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,17 @@
-import { HeroUIProvider } from "@heroui/react"
+"use client"
+
+import { HeroUIProvider, ToastProvider } from "@heroui/react"
 import { ThemeProvider } from "next-themes"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
+      {/* Provedor para notificações (toast) do HeroUI */}
+      <ToastProvider
+        placement="top-right" //posição do toast
+        toastOffset={8} // deslocamento
+      />
+      {/* Provedor de tema com o elemento filho */}
       <ThemeProvider attribute="class">{children}</ThemeProvider>
     </HeroUIProvider>
   )
